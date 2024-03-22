915e479be4f48976f62389fc746d0622df5ac3e4
Eden Chan <edenchan717@gmail.com>
2024-03-22 19:15:03 -0400

    fix: revert to working version of Tip

--- apps/web/app/pdf/ui/components/Tip.tsx
+++ apps/web/app/pdf/ui/components/Tip.tsx
@@ -1,7 +1,12 @@
-'use client'
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 
 import "../style/Tip.css";
+
+interface State {
+  compact: boolean;
+  text: string;
+  emoji: string;
+}
 
 interface Props {
   onConfirm: (comment: { text: string; emoji: string }) => void;
@@ -9,72 +14,85 @@ interface Props {
   onUpdate?: () => void;
 }
 
-export const Tip: React.FC<Props> = ({ onConfirm, onOpen, onUpdate }) => {
-  const [compact, setCompact] = useState(true);
-  const [text, setText] = useState("");
-  const [emoji, setEmoji] = useState("");
+export class Tip extends Component<Props, State> {
+  state: State = {
+    compact: true,
+    text: "",
+    emoji: "",
+  };
 
-  useEffect(() => {
-    if (onUpdate) {
+  // for TipContainer
+  componentDidUpdate(nextProps: Props, nextState: State) {
+    const { onUpdate } = this.props;
+
+    if (onUpdate && this.state.compact !== nextState.compact) {
       onUpdate();
     }
-  }, [compact, onUpdate]);
+  }
 
-  return (
-    <div className="Tip">
-      {compact ? (
-        <div
-          className="Tip__compact"
-          onClick={() => {
-            onOpen();
-            setCompact(false);
-          }}
-        >
-          Add highlight
-        </div>
-      ) : (
-        <form
-          className="Tip__card"
-          onSubmit={(event) => {
-            event.preventDefault();
-            onConfirm({ text, emoji });
-          }}
-        >
-          <div>
-            <textarea
-              placeholder="Your comment"
-              autoFocus
-              value={text}
-              onChange={(event) => setText(event.target.value)}
-              ref={(node) => {
-                if (node) {
-                  node.focus();
-                }
-              }}
-            />
+  render() {
+    const { onConfirm, onOpen } = this.props;
+    const { compact, text, emoji } = this.state;
+
+    return (
+      <div className="Tip">
+        {compact ? (
+          <div
+            className="Tip__compact"
+            onClick={() => {
+              onOpen();
+              this.setState({ compact: false });
+            }}
+          >
+            Add highlight
+          </div>
+        ) : (
+          <form
+            className="Tip__card"
+            onSubmit={(event) => {
+              event.preventDefault();
+              onConfirm({ text, emoji });
+            }}
+          >
             <div>
-              {["💩", "😱", "😍", "🔥", "😳", "⚠️"].map((_emoji) => (
-                <label key={_emoji}>
-                  <input
-                    checked={emoji === _emoji}
-                    type="radio"
-                    name="emoji"
-                    value={_emoji}
-                    onChange={(event) => setEmoji(event.target.value)}
-                  />
-                  {_emoji}
-                </label>
-              ))}
+              <textarea
+                placeholder="Your comment"
+                autoFocus
+                value={text}
+                onChange={(event) =>
+                  this.setState({ text: event.target.value })
+                }
+                ref={(node) => {
+                  if (node) {
+                    node.focus();
+                  }
+                }}
+              />
+              <div>
+                {["💩", "😱", "😍", "🔥", "😳", "⚠️"].map((_emoji) => (
+                  <label key={_emoji}>
+                    <input
+                      checked={emoji === _emoji}
+                      type="radio"
+                      name="emoji"
+                      value={_emoji}
+                      onChange={(event) =>
+                        this.setState({ emoji: event.target.value })
+                      }
+                    />
+                    {_emoji}
+                  </label>
+                ))}
+              </div>
             </div>
-          </div>
-          <div>
-            <input type="submit" className="text-black" value="Save" />
-          </div>
-        </form>
-      )}
-    </div>
-  );
-};
+            <div>
+              <input type="submit" className="text-black" value="Save" />
+            </div>
+          </form>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Tip;
-
