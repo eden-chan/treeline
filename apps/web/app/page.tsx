19e5237213ae5a0cb9c31750a186e2fe61921b97
LemonFace0309 <charles.liu0309@gmail.com>
2024-03-22 01:40:25 -0400

    WE SEE THE NODES

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,18 +1,10 @@
-import React from "react";
-import ReactFlow from "reactflow";
+import Tree from "../components/tree";
+import styles from "./page.module.css";
 
-import "reactflow/dist/style.css";
-
-const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
-export default function App() {
+export default function Page(): JSX.Element {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
-    </div>
+    <main className={styles.main}>
+      <Tree />
+    </main>
   );
 }
