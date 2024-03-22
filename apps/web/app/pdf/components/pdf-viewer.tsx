53e88222cbb6c4d326712306fb5a68db1158c0ac
Eden Chan <edenchan717@gmail.com>
2024-03-22 02:06:01 -0400

    render all pages with highlightable text

--- apps/web/app/pdf/components/pdf-viewer.tsx
+++ apps/web/app/pdf/components/pdf-viewer.tsx
@@ -1,31 +1,74 @@
 
 'use client';
 
-import { pdfjs } from 'react-pdf';
+import { useCallback, useState } from 'react';
+import { useResizeObserver } from '@wojtekmaj/react-hooks';
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import type { PDFDocumentProxy } from 'pdfjs-dist';
+
 
 // pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 //   'pdfjs-dist/build/pdf.worker.min.js',
 //   import.meta.url,
 // ).toString();
-
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
-import { useState } from 'react';
-import { Document, Page } from 'react-pdf';
+const options = {
+    cMapUrl: '/cmaps/',
+    standardFontDataUrl: '/standard_fonts/',
+  };
+  const resizeObserverOptions = {};
 
+  const maxWidth = 800;
+  
+  type PDFFile = string | File | null;
+
+  
 export default function PDFViewer() {
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState<number>(1);
+  const [file, setFile] = useState<PDFFile>('https://arxiv.org/pdf/1706.03762.pdf');
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  function onFileChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const { files } = event.target;
+
+    if (files && files[0]) {
+      setFile(files[0] || null);
+    }
   }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
+ 
 
   return (
     <div>
-      <Document file="https://arxiv.org/pdf/1706.03762.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+      <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
+        {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+              />
+            ))}
       </Document>
       <p>
         Page {pageNumber} of {numPages}
