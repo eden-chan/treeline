24b270dc54ed816aaea4841b6cc1c31bbf3d96ca
LemonFace0309 <charles.liu0309@gmail.com>
2024-03-22 01:50:19 -0400

    some updates to styling

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,8 @@
 import Tree from "../components/tree";
-import styles from "./page.module.css";
 
 export default function Page(): JSX.Element {
   return (
-    <main className={styles.main}>
+    <main>
       <Tree />
     </main>
   );
