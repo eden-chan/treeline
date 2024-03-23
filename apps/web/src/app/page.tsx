ae9569b4ce43ec7f1c8d683434d147a7d6904970
Eden Chan <edenchan717@gmail.com>
2024-03-22 22:51:14 -0400

    add trpc

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Tree from "~/components/tree";
 import PDFViewer from "~/components/pdf-viewer";
+import { api } from "~/trpc/server";
 
-export default function Page(): JSX.Element {
+export default async function Page() {
+  const hello = await api.post.hello({ text: "from tRPC" });
   return (
     <main className="h-screen w-screen grid grid-cols-2 gap-0">
-      <PDFViewer />
-      <Tree />
+      <h1>{hello.greeting}</h1>
+      {/* <PDFViewer /> */}
+      {/* <Tree /> */}
     </main>
   );
 }
