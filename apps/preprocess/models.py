38d5a0e6b4a4fa885d5907b2fc8bfa6fc676740f
Eden Chan <edenchan717@gmail.com>
2024-04-13 18:30:35 -0400

    extract abstract and title from paper

--- apps/preprocess/models.py
+++ apps/preprocess/models.py
@@ -1,8 +1,9 @@
 
 from typing import List
-from pydantic import BaseModel, Field, conlist
+from pydantic import BaseModel, Field, conlist, Field, BeforeValidator
 from enum import Enum, auto
-
+from typing_extensions import Annotated
+import instructor
 # Define data models using Pydantic for type validation and settings
 class User(BaseModel):
     name: str
@@ -25,8 +26,47 @@ class FactDescriptor(BaseModel):
             }
         }
 
+
 class ExtractFactDescriptor(BaseModel):
     fact_descriptors : List[FactDescriptor]=  Field()
+
+
+class RankedTitle(BaseModel):
+    reasoning: str = Field(description='Is this candidate text the title of the paper?  Is it concise (around 10-15 words), informative, and reflective of the paper\'s content?')
+    confidence: float = Field(
+        ge=0,
+        le=1,
+        description="The confidence of the candidate being the title based on the reasoning, 0 is low, 1 is high",
+    )
+    candidate: str = Field(description='The original text of the candidate title')
+
+class TitleRanker(BaseModel):
+    ranked_candidates: List[RankedTitle]
+    answer: Annotated[
+        str,
+        BeforeValidator(
+            instructor.llm_validator("ensure titles don't contain objectionable content", allow_override=True)
+        ),
+    ]
+    
+class RankedAbstract(BaseModel):
+    reasoning: str = Field(description='Is this candidate text the research paper abstract? Is it prefaced by ABSTRACT? Does it briefly summarize the paper\'s purpose, research problem, methodology, major findings, conclusions, and implications?')
+    confidence: float = Field(
+        ge=0,
+        le=1,
+        description="The confidence of the candidate being the abstract based on the reasoning, 0 is low, 1 is high",
+    )
+    candidate: str = Field(description='The candidate abstract')
+
+class AbstractRanker(BaseModel):
+    ranked_candidates: List[RankedAbstract]
+    answer: Annotated[
+        str,
+        BeforeValidator(
+            instructor.llm_validator("ensure abstracts don't contain objectionable content", allow_override=True)
+        ),
+    ]
+    
 
 
 
--- apps/preprocess/pdf_parser_server.py
+++ apps/preprocess/pdf_parser_server.py
@@ -1,9 +1,10 @@
+from typing import List
 from db import mongo_client
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, BeforeValidator
 import time
 import os
-from models import ExtractFactDescriptor, FactDescriptor, ClientType
+from models import AbstractRanker, ExtractFactDescriptor, FactDescriptor, ClientType, TitleRanker
 import instructor
 from openai import OpenAI
 
@@ -16,8 +17,53 @@ from llama_index.core.node_parser import MarkdownNodeParser
 
 
 
+
 client = instructor.patch(OpenAI())
 
+
+def extract_title(title_candidates: List[str]):
+    try:
+        qa: TitleRanker = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            max_retries=2,
+            response_model=TitleRanker,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are an expert at identifying the most likely title for research papers from a list of sentences. Select the sentence that is most likely to be the title.",
+                },
+                {
+                    "role": "user",
+                    "content": f"Here are the title candidates: {title_candidates}\n\n Rank each candidate by how likely it's the title of the paper.",
+                },
+            ],
+        )
+    except Exception as e:
+        print(e)
+    highest_confidence_title = max(qa.ranked_candidates, key=lambda x: x.confidence)
+    return highest_confidence_title.candidate
+
+def extract_abstract(abstract_candidates: List[str]):
+    try:
+        qa: AbstractRanker = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            max_retries=2,
+            response_model=AbstractRanker,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are an expert at identifying the most likely abstract for research papers from a list of candidate text. A research paper abstract is usually prefaced by ABSTRACT. If it\s not prefaced by ABSTRACT it is unlikely to be the abstract. It should briefly summarize the paper\'s purpose, research problem, methodology, major findings, conclusions, and implications. The candidate text can also not be relevant at all to the abstract. Select candidate that is most likely to be the abstract.",
+                },
+                {
+                    "role": "user",
+                    "content": f"Here are the abstract candidates: {abstract_candidates}\n\n Rank each candidate by how likely it's the title of the paper.",
+                },
+            ],
+        )
+    except Exception as e:
+        print(e)
+    highest_confidence_abstract = max(qa.ranked_candidates, key=lambda x: x.confidence)
+    return highest_confidence_abstract.candidate
 
 
 def chat(user_text, response_model):
@@ -44,11 +90,17 @@ async def parse_pdf(pdf_url):
     # Use httpx.AsyncClient for asynchronous HTTP requests
     async with httpx.AsyncClient() as client:
         response = await client.get(pdf_url)
-
-    # Assuming you have a temporary file setup as before
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
-        temp_file.write(response.content)
-        temp_pdf_path = temp_file.name
+        
+    if not response.headers["Content-Type"] == "application/pdf":
+        raise HTTPException(status_code=400, detail="The provided URL does not point to a PDF document.")
+    
+    try:
+        # Assuming you have a temporary file setup as before
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
+            temp_file.write(response.content)
+            temp_pdf_path = temp_file.name
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to create a temporary PDF file: {str(e)}")
 
     # Correctly await the asynchronous call to LlamaParse.aload_data
     documents = await LlamaParse(result_type="markdown").aload_data(file_path=temp_pdf_path)
@@ -60,19 +112,25 @@ async def parse_pdf(pdf_url):
 
     with ThreadPoolExecutor() as executor:
         try:
+            # there can be preface text, so we take the first 5 sections as potential title candidates
+            candidate_titles = [node.text.split('\n')[0] for node in nodes[:5]]
+            
+            # there can be preface text, so we take the second to fourth sections as potential candidates
+            candidate_abstracts = [node.text for node in nodes[0:5]]
+            title = extract_title(candidate_titles)
+            abstract = extract_abstract(candidate_abstracts)
+
             futures = [executor.submit(chat, node.text, ExtractFactDescriptor) for node in nodes]
             parsing = [future.result() for future in as_completed(futures)]
-            # Ensure proper serialization of the facts
-            # import pdb; pdb.set_trace()
+
             facts = [parsing_item.fact_descriptors for parsing_item in parsing]
             facts_serialized = [fact_descriptor.dict() for fact in facts for fact_descriptor in fact]
-            title = nodes[0].text.split('\n')[0]
             
             sections = [{'text': node.text, 'metadata': node.metadata} for node in nodes]
             
             with mongo_client(db_name='paper', collection_name='ParsedPapers') as db: 
                 filter_query = {"source": pdf_url}
-                update_data = {"$set": {"title": title, "facts": facts_serialized, 'sections': sections}}
+                update_data = {"$set": {"title": title, "abstract": abstract, "facts": facts_serialized, 'sections': sections}}
                 result = db.update_one(filter_query, update_data, upsert=True)
         except Exception as e:
             print(f"An error occurred during parsing or database update: {e}")
@@ -95,7 +153,7 @@ async def parse_pdf(pdf_url):
             mongo_id = None
     
     
-    response = {"source": pdf_url, 'sections': sections, 'facts': facts_serialized, 'title': title, "mongo_id": mongo_id }
+    response = {"source": pdf_url, 'sections': sections, 'facts': facts_serialized, 'title': title, "abstract": abstract, "mongo_id": mongo_id }
     return response
     
 app = FastAPI()
