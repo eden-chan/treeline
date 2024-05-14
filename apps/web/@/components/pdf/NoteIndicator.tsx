950c365b4dfc1eae44192d04f261e9f8dab11841
Eden Chan <edenchan717@gmail.com>
2024-05-13 23:26:33 -0700

    make note popup scrollable

--- apps/web/@/components/pdf/NoteIndicator.tsx
+++ apps/web/@/components/pdf/NoteIndicator.tsx
@@ -61,50 +61,50 @@ export const NoteIndicator = ({ note, rightmostArea }) => {
                 onMouseEnter={handleAsteriskHover}
                 onMouseLeave={handleAsteriskLeave}
             >
-                ¶ {createPortal(
+                ¶
+                {createPortal(
                     <span
                         className={`z-40 ${isHovered ? 'block' : 'hidden'} absolute bg-white text-black p-2 rounded shadow-lg text-sm break-all max-w-xs transition-opacity duration-200 overflow-visible flex flex-col`}
                         style={{
                             left: `${initialPosition.x - 20}px`,
                             top: `${initialPosition.y + 40}px`,
                             transform: 'translate(16px, -50%)',
-                            width: '200px',
+                            width: '300px',
+                            height: '200px',
+                            overflowY: 'scroll',
                         }}
                         onMouseEnter={() => setIsQuoteHovered(true)}
                         onMouseLeave={() => setIsQuoteHovered(false)}
                     >
-                        <div className="flex flex-col relative">
-
-                            <div className="flex justify-start items-center">
-                                <div className="flex items-center space-x-1">
-                                    <button
-                                        className="text-blue-500 hover:text-blue-700 p-1"
-                                        onClick={handleEdit}
-                                    >
-                                        <Pencil size={16} />
-                                    </button>
-                                    <button
-                                        className="text-blue-500 hover:text-blue-700 p-1"
-                                        onClick={handleTrash}
-                                    >
-                                        <Trash2 size={16} />
-                                    </button>
-                                    <button
-                                        className="text-blue-500 hover:text-blue-700 p-1"
-                                        onClick={handleComment}
-                                    >
-                                        <MessageCircle size={16} />
-                                    </button>
+                        <div className="flex flex-col h-full">
+                            <div className="sticky top-0 left-0 bg-white z-10 py-2">
+                                <div className="flex justify-start items-center">
+                                    <div className="flex items-center space-x-1">
+                                        <button
+                                            className="text-blue-500 hover:text-blue-700 p-1"
+                                            onClick={handleEdit}
+                                        >
+                                            <Pencil size={16} />
+                                        </button>
+                                        <button
+                                            className="text-blue-500 hover:text-blue-700 p-1"
+                                            onClick={handleTrash}
+                                        >
+                                            <Trash2 size={16} />
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                            <span className="ml-6">{note.quote}</span>
-                            <div className="flex justify-end mt-2">
-                                <button
-                                    className="text-blue-500 hover:text-blue-700 font-semibold"
-                                    onClick={handleReply}
-                                >
-                                    Reply
-                                </button>
+                            <span className="overflow-auto h-full">{note.quote}</span>
+                            <div className="sticky bottom-0 left-0 bg-white z-10">
+                                <div className="flex justify-end">
+                                    <button
+                                        className="text-blue-500 hover:text-blue-700 font-semibold text-sm"
+                                        onClick={handleReply}
+                                    >
+                                        Reply
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </span>,
